6aac72c79ea449a86fb68db8eaff01579ff7c9d6
maxkeller321 <max-knobloch2312@outlook.de>
2021-05-14 09:24:11 +0200

    Added data loader skeleton

--- CNN-AI-CT/CNN_ai-ct.py
+++ CNN-AI-CT/CNN_ai-ct.py
@@ -2,9 +2,10 @@ import os
 import torch
 from torch import nn
 import torch.nn.functional as F
+from torch.utils.data import Dataset
 from torchvision import transforms
 from torchvision.datasets import MNIST
-#from torchvision import datasets
+from torchvision import datasets
 from torch.utils.data import DataLoader, random_split
 import pytorch_lightning as pl
 import numpy as np
@@ -13,6 +14,74 @@ from PIL import Image
 device = "cuda" if torch.cuda.is_available() else "cpu"
 number_of_nodes = 1 
 number_of_gpus = 0 
+batch_size = 12
+
+
+class ConcatDataset(torch.utils.data.Dataset):
+    def __init__(self, *datasets):
+        self.datasets = datasets
+
+    def __getitem__(self, i):
+        return tuple(d[i] for d in self.datasets)
+
+    def __len__(self):
+        return min(len(d) for d in self.datasets)
+
+class SliceDataset(Dataset):
+    """Face Landmarks dataset."""
+
+    def __init__(self, file_path, transform=None):
+        """
+        Args:
+            file_path (string): Path to the volume data.
+            transform (callable, optional): Optional transform to be applied
+                on a sample.
+        """
+        self.file_path = file_path
+        self.transform = transform
+
+    def __len__(self):
+        """
+        # load here maybe data and return the first dimension - 4 (the outer most four slices are not usefull)
+        We do also have to take care for the patches of size 256 x 256 x 5
+
+        """
+        return len("test")
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        """
+            supply here the file reading and slice selection (Select always also the neighbouring 4 slices)
+            The index 0 should correspond to the middle slice number 3 (counted from 1) and the highest index to the middle slice __len__ + 2.
+            We do also have to take care for the patches of size 256 x 256 x 5
+        """
+        sample = "test"
+
+        if self.transform:
+            sample = self.transform(sample)
+
+        return sample
+
+train_loader = torch.utils.data.DataLoader(
+             ConcatDataset(
+                 SliceDataset(r"\test"), # Supply here all Volume paths
+                 SliceDataset(r"\test2"),
+             ),
+             batch_size=batch_size,
+             shuffle=True,
+             num_workers=number_of_gpus,
+             pin_memory=True, # loads them directly in cuda pinned memory 
+             drop_last=True) # drop the last incomplete batch
+
+
+
+
+for idx, input_seq in enumerate(train_loader):
+    pass 
+
+
 
 class CNN_AICT(pl.LightningModule):
 
