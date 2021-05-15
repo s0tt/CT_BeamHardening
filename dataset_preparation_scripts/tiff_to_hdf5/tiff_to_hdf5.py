2a3ae4ad645f9da2ceed86b08cec8509ffbf4325
Stefan Ott <kicker17@gmx.de>
2021-05-15 22:54:52 +0200

    Add HDF5 script chunk wise resizing to save RAM

--- dataset_preparation_scripts/tiff_to_hdf5/tiff_to_hdf5.py
+++ dataset_preparation_scripts/tiff_to_hdf5/tiff_to_hdf5.py
@@ -28,12 +28,14 @@ distance_source_axis = 0.00363074699401855 # Source to Object-Stage (Center of r
 distance_source_detector = 1.229268# [m]
 
 
-def hdf5_tiff_builder(file_name: str, angle: int, detector_pixel_size: float,
+def hdf5_tiff_builder(file_name: str,  detector_pixel_size: float,
                      distance_source_axis: float, distance_source_detector: float,
-                    dimension, image, type_data): 
+                    dimension, image_paths, type_data): 
                      
    with h5py.File(file_name, "w") as f:
-      angle = f.create_dataset("Angle", data=angle, dtype='float64')
+      # angles are in rad. start at 0 and go to shortly before 6.28 .. (2*pi)
+      angle_rad = np.linspace(0, 2*np.pi, load_image_in_numpy_array(image_paths[0]).shape[0], dtype=np.float64) 
+      angle = f.create_dataset("Angle", data=angle_rad, dtype='float64')
       angle.attrs['MATLAB_class'] = 'double'
       f["DetectorPixelSizeX"] = detector_pixel_size
       f["DetectorPixelSizeX"].attrs['MATLAB_class'] = 'double'
@@ -49,39 +51,45 @@ def hdf5_tiff_builder(file_name: str, angle: int, detector_pixel_size: float,
       f["DistanceSourceAxis"].attrs['MATLAB_class'] = 'double'
       f["DistanceSourceDetector"] = distance_source_detector
       f["DistanceSourceDetector"].attrs['MATLAB_class'] = 'double'
-      image = f.create_dataset("Image", data=image, dtype='float64')
-      image.attrs['MATLAB_class'] = 'double'    
+
+      image_dataset = None
+      for img_path in image_paths:
+         if image_dataset is None:
+            print("Create image dataset with: ", img_path)
+            image_dataset = f.create_dataset("Image", data=load_image_in_numpy_array(img_path), dtype='float64', chunks=True, maxshape=(None,None) )
+         else:
+            print("Appending image: ", img_path)
+            img_current = load_image_in_numpy_array(img_path)
+            image_dataset.resize(image_dataset.shape[0]+img_current.shape[0], axis=0)
+            image_dataset[-img_current.shape[0]:] = img_current
+      image_dataset.attrs['MATLAB_class'] = 'double'    
       type_ = f.create_dataset("Type", data=type_data, dtype='uint16')
       type_.attrs['MATLAB_class'] = 'char'
       type_.attrs['MATLAB_int_decode'] = 2
 
 
-def load_images_in_numpy_array(image_path): 
-   images = []
-
-   for root, dirs, files in os.walk(image_path):
-      for name in sorted(files):
+def load_image_in_numpy_array(image_path): 
          try: 
-            images.append(np.array(Image.open(os.path.join(root, name)), dtype=np.float64)) 
+            return np.array(Image.open(image_path), dtype=np.float64)
          except: 
-            raise Exception("Folder does contain files which are no readable images")
+            raise Exception("image path not readable")
 
-   stacked_images = np.stack(images)
-   return stacked_images
-
+def get_image_list(image_dir):
+   img_list = []
+   for root, dirs, files in os.walk(image_dir):
+      for name in sorted(files):
+         img_list.append(os.path.join(root, name))
+   return img_list
 
 
 # extracted and saved them from the CT/Matlab_KSK/ReferenceProjections.hdf5 in ctutils 
 dimension_data = np.loadtxt(os.path.join('parameter_files', 'dimension_hdf5.txt'), delimiter=',')
 type_data = np.loadtxt(os.path.join('parameter_files', 'type_hdf5.txt'), delimiter=',')
 
-images = load_images_in_numpy_array(args.tiff_files_path)
+img_paths = get_image_list(args.tiff_files_path)
 
 
-# angles are in rad. start at 0 and go to shortly before 6.28 .. (2*pi)
-angle = np.linspace(0, 2*np.pi, images.shape[0], dtype=np.float64) 
-
-hdf5_tiff_builder(args.output_absolute_path, angle, detector_pixel_size,
+hdf5_tiff_builder(args.output_absolute_path, detector_pixel_size,
                    distance_source_axis, distance_source_detector, dimension_data,
-                   images, type_data)
+                   img_paths, type_data)
 
