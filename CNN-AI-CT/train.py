d17590c37d37d5b3d3c8e7023bccf720bc572134
Stefan <ottsn@studi.informatik.uni-stuttgart.de>
2021-06-02 14:34:17 +0200

    Switch to lightning train argparse

--- CNN-AI-CT/train.py
+++ CNN-AI-CT/train.py
@@ -20,29 +20,27 @@ def main():
                         help="Path to input volume for training")
     parser.add_argument("--file-gt", "-gt", required=True,
                         help="Path to input volume ground truth file")
-    parser.add_argument("--nr_gpus", "-g", required=False, default=0,
-                        help="number of gpus to use")
     parser.add_argument("--nr_workers", "-w", required=False, default=2,
                         help="number of worker subproccesses to prefetch")
-    parser.add_argument("--nr_nodes", "-n", required=False, default=1,
-                        help="Path to input volume ground truth file")
+    parser = pl.Trainer.add_argparse_args(parser)
 
     args = parser.parse_args()
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    number_of_nodes = args.nr_nodes # number of GPU nodes for distributed training.
-    number_of_gpus = args.nr_gpus # number of gpus to train on (int) or which GPUs to train on (list or str) applied per node
-    
     # Accelerator
     # 'ddp': multiple-gpus across many machines (python script based))
     # 'dp' : is DataParallel (split batch among GPUs of same machine)
-    accelerator_type = "dp"
+    accelerator_type = args.accelerator
     num_workers = args.nr_workers
     batch_size = 16
     dataset_stride = 128 
     num_pixel = 256 
     test_split = 0.3
+    val_split = 0.2
     dataset_paths = [(args.file_in, args.file_gt)] 
+    number_of_nodes = args.num_nodes # number of GPU nodes for distributed training.
+    number_of_gpus = args.gpus # number of gpus to train on (int) or which GPUs to train on (list or str) applied per node
+    max_epochs = args.max_epochs # max number of epochs
 
     # initialize tesnorboard logger
     tb_logger = TensorBoardLogger('logs/')
@@ -65,9 +63,8 @@ def main():
     )
 
     # train model
-    trainer = pl.Trainer(
-        gpus=number_of_gpus, 
-        num_nodes=number_of_nodes, 
+    trainer = pl.Trainer.from_argparse_args(
+        parser, 
         logger=tb_logger, 
         accelerator=accelerator_type,
         callbacks=[train_loss_callback, val_loss_callback]
@@ -77,7 +74,16 @@ def main():
     #cli = LightningCLI(LitClassifier, MNISTDataModule, seed_everything_default=1234)
     #result = cli.trainer.test(cli.model, datamodule=cli.datamodule)
 
-    ct_volumes = CtVolumeData(paths= dataset_paths)
+    # CT data loading
+    ct_volumes = CtVolumeData(
+        paths=dataset_paths, 
+        batch_size=batch_size,
+        num_workers=num_workers,
+        dataset_stride = dataset_stride, 
+        num_pixel = num_pixel,
+        test_split = test_split,
+        val_split = val_split
+        )
 
     # init model
     ref_img, ref_label = next(iter(ct_volumes.test_dataloader()))
