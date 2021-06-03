760ac4c6147bfe6883bae828966d4999f83bc29d
kellermx <kellermx@ipvs.uni-stuttgart.de>
2021-06-03 15:48:33 +0200

    reco_post_processing refactorings

--- dataset_preparation_scripts/reco_post_processing.py
+++ dataset_preparation_scripts/reco_post_processing.py
@@ -9,39 +9,25 @@ gv_factor = 1 # scaling factor for the mean grey value
 
 def trans_hdf5_incremental(path_in: str, path_out: str):
     with h5py.File(path_in, "r") as f_in:
-        if path_out is not None:
-            name_new = path_out
-        else:
-            name_old = path_in.split(".hdf5")[0]
-            name_new = name_old+"_transposed.hdf5"
-        with h5py.File(name_new, "w") as f_out:
-            new_volume_dataset = None
+        with h5py.File(path_out, "w") as f_out:
 
-            # takeover all other existing groups beside "Volume"
-            for key in f_in.keys():
-                if key != "Volume":
-                    # Get parent group name for copy
-                    group_path = f_in[key].parent.name
-                    # Check existence of group, else create group+parent
-                    group_id = f_out.require_group(group_path)
-                    f_in.copy(key, group_id, group_path+key)
-            
             vol_hdf5 = f_in["Volume"]
+            new_volume_dataset = None 
+
             # Volume iteration loop
             grey_value_sum = 0
             for y_slice in range(vol_hdf5.shape[1]):
-                vol_data = vol_hdf5[:, y_slice, :]
-                grey_value_sum += vol_data.flatten().sum()
+                vol_slice = vol_hdf5[:, y_slice, :]
+                grey_value_sum += vol_slice.flatten().sum()
 
-                vol_data_3d = np.expand_dims(vol_data, axis=0)
                 if new_volume_dataset is None:
+                    vol_slice = np.expand_dims(vol_slice, axis=0)
                     new_volume_dataset = f_out.create_dataset("Volume",
-                        data=vol_data_3d, dtype=DATATYPE_USED, chunks=True,
-                        maxshape=(None, None, None))
+                    data=vol_slice, dtype=DATATYPE_USED, chunks=True,
+                    maxshape=(None, None, None))
                 else:
-                    new_volume_dataset.resize(new_volume_dataset.shape[0] +
-                        vol_data_3d.shape[0], axis=0)
-                    new_volume_dataset[y_slice, :, :] = vol_data_3d
+                    new_volume_dataset.resize(new_volume_dataset.shape[0] + 1, axis=0)
+                    new_volume_dataset[y_slice, :, :] = vol_slice
             
             new_volume_dataset.attrs['MATLAB_class'] = 'double'
             new_volume_dataset.attrs['Mean_grey_value'] = grey_value_sum/vol_hdf5.size
@@ -57,39 +43,34 @@ def cut_volume_by_axis(axis: int, cuts_axis: tuple, path_old: str, path_new: str
     """
     with h5py.File(path_old, "r+") as f_in: 
         with h5py.File(path_new, "w") as f_out:
-            for key in f_in.keys():
-                if key != "Volume":
-                    # Get parent group name for copy
-                    group_path = f_in[key].parent.name
-                    # Check existence of group, else create group+parent
-                    group_id = f_out.require_group(group_path)
-                    f_in.copy(key, group_id, group_path+key)
-            
+
             vol_hdf5 = f_in["Volume"]
+            new_volume_dataset = None
+
             for slice_nr in range(cuts_axis[0], vol_hdf5.shape[axis]):
                 if axis == 0: 
-                    vol_data = vol_hdf5[slice_nr, :, :]
+                    vol_slice = vol_hdf5[slice_nr, :, :]
                 elif axis == 1: 
-                    vol_data = vol_hdf5[:, slice_nr, :]
+                    vol_slice = vol_hdf5[:, slice_nr, :]
                 elif axis == 2: 
-                    vol_data = vol_hdf5[:, :, slice_nr]
-               
-                vol_data = np.expand_dims(vol_data, axis=axis)
+                    vol_slice = vol_hdf5[:, :, slice_nr]
+
                 if new_volume_dataset is None:
-                        new_volume_dataset = f_out.create_dataset("Volume",
-                        data=vol_data, dtype=DATATYPE_USED, chunks=True,
-                        maxshape=(None, None, None))
+                    vol_slice = np.expand_dims(vol_slice, axis=axis)
+                    new_volume_dataset = f_out.create_dataset("Volume",
+                    data=vol_slice, dtype=DATATYPE_USED, chunks=True,
+                    maxshape=(None, None, None))
                 else:
                     shape_list = list(new_volume_dataset.shape)
                     shape_list[axis] = shape_list[axis] + 1 
                     new_volume_dataset.resize(tuple(shape_list))
 
                     if axis == 0: 
-                        new_volume_dataset[slice_nr-cuts_axis[0], :, :] = vol_data
+                        new_volume_dataset[slice_nr-cuts_axis[0], :, :] = vol_slice
                     elif axis == 1: 
-                        new_volume_dataset[:, slice_nr-cuts_axis[0], :] = vol_data
+                        new_volume_dataset[:, slice_nr-cuts_axis[0], :] = vol_slice
                     elif axis == 2: 
-                        new_volume_dataset[:, :, slice_nr-cuts_axis[0]] = vol_data
+                        new_volume_dataset[:, :, slice_nr-cuts_axis[0]] = vol_slice
             
             # removes end slices
             new_volume_dataset.resize(new_volume_dataset.shape[axis]-cuts_axis[1], axis=axis)
@@ -97,7 +78,18 @@ def cut_volume_by_axis(axis: int, cuts_axis: tuple, path_old: str, path_new: str
             new_volume_dataset.attrs['MATLAB_class'] = 'double'
 
 
-def cut_air_slices_by_axis(axis: int, path_old: str, path_new:str): 
+def cut_air_slices_by_axis(axis: int, path_old: str, path_new:str) -> tuple:  
+    """
+        removes the edge slices in both directions perpendicular to the passed axis, 
+        where the mean grey value is lower than the grey value of the full volume 
+        multiplied with a scaling factor:  
+            (mean_grey_value*gv_factor > mean_grey_value_slice)
+
+        @return: 
+            (slices_start, slices_end): 
+                slices_start:  number of removed slices in the beginning 
+                slices_end: number of removed slices in the end
+    """
 
     if axis not in [0, 1, 2]: 
         raise Exception("Axis must be either 0, 1 or 2")
@@ -105,26 +97,20 @@ def cut_air_slices_by_axis(axis: int, path_old: str, path_new:str):
     with h5py.File(path_old, "r+") as f_in:
         mean_grey_value = f_in["Volume"].attrs['Mean_grey_value']
         with h5py.File(path_new, "w") as f_out:
-            for key in f_in.keys():
-                if key != "Volume":
-                    # Get parent group name for copy
-                    group_path = f_in[key].parent.name
-                    # Check existence of group, else create group+parent
-                    group_id = f_out.require_group(group_path)
-                    f_in.copy(key, group_id, group_path+key)
-            
+
             vol_hdf5 = f_in["Volume"]
             first_relevant_slice = False
             new_volume_dataset = None
+
             for slice_nr in range(vol_hdf5.shape[axis]):
                 if axis == 0: 
-                    vol_data = vol_hdf5[slice_nr, :, :]
+                    vol_slice = vol_hdf5[slice_nr, :, :]
                 elif axis == 1: 
-                    vol_data = vol_hdf5[:, slice_nr, :]
+                    vol_slice = vol_hdf5[:, slice_nr, :]
                 elif axis == 2: 
-                    vol_data = vol_hdf5[:, :, slice_nr]
+                    vol_slice = vol_hdf5[:, :, slice_nr]
 
-                mean_grey_value_slice = (vol_data.flatten().sum())/vol_data.size
+                mean_grey_value_slice = (vol_slice.flatten().sum())/vol_slice.size
 
                 if not first_relevant_slice and (mean_grey_value*gv_factor > mean_grey_value_slice):
                     pass
@@ -132,34 +118,34 @@ def cut_air_slices_by_axis(axis: int, path_old: str, path_new:str):
                     if not first_relevant_slice:
                         slices_start = slice_nr
                         first_relevant_slice = True 
-                        subtract = slice_nr
-                        vol_data = np.expand_dims(vol_data, axis=axis)
+
                     if new_volume_dataset is None:
-                            new_volume_dataset = f_out.create_dataset("Volume",
-                            data=vol_data, dtype=DATATYPE_USED, chunks=True,
-                            maxshape=(None, None, None))
-                    else:
+                        vol_slice = np.expand_dims(vol_slice, axis=axis)
+                        new_volume_dataset = f_out.create_dataset("Volume",
+                        data=vol_slice, dtype=DATATYPE_USED, chunks=True,
+                        maxshape=(None, None, None))
+                    else: 
                         shape_list = list(new_volume_dataset.shape)
                         shape_list[axis] = shape_list[axis] + 1 
                         new_volume_dataset.resize(tuple(shape_list))
 
                         if axis == 0: 
-                            new_volume_dataset[slice_nr-subtract, :, :] = vol_data
+                            new_volume_dataset[slice_nr-slices_start, :, :] = vol_slice
                         elif axis == 1: 
-                            new_volume_dataset[:, slice_nr-subtract, :] = vol_data
+                            new_volume_dataset[:, slice_nr-slices_start, :] = vol_slice
                         elif axis == 2: 
-                            new_volume_dataset[:, :, slice_nr-subtract] = vol_data
+                            new_volume_dataset[:, :, slice_nr-slices_start] = vol_slice
                         
             
             # removes end slices
             for slice_nr in range(new_volume_dataset.shape[axis]):
                 if axis == 0: 
-                    vol_data = new_volume_dataset[-1, :, :]
+                    vol_slice = new_volume_dataset[-1, :, :]
                 elif axis == 1: 
-                    vol_data = new_volume_dataset[:, -1, :]
+                    vol_slice = new_volume_dataset[:, -1, :]
                 elif axis == 2: 
-                    vol_data = new_volume_dataset[:, :, -1]
-                mean_slice_grey_value = vol_data.flatten().sum()/vol_data.size
+                    vol_slice = new_volume_dataset[:, :, -1]
+                mean_slice_grey_value = vol_slice.flatten().sum()/vol_slice.size
                 if mean_slice_grey_value < mean_grey_value*gv_factor: 
                     new_volume_dataset.resize(new_volume_dataset.shape[axis]-1, axis=axis)
                 else: 
@@ -179,6 +165,7 @@ def cut_volume(path_in_poly: str, path_in_mono: str):
         the grey values of the poly volume
     """
 
+    
     # Poly part 
     name_old_poly = path_in_poly.split(".hdf5")[0]
     name_new_0_poly = name_old_poly+"_dim_0_reduced.hdf5"
@@ -190,42 +177,44 @@ def cut_volume(path_in_poly: str, path_in_mono: str):
     os.remove(name_new_0_poly)
     cuts_z = cut_air_slices_by_axis(2, name_new_1_poly, path_in_poly)
     os.remove(name_new_1_poly)
-  
+    
+
     # Mono part 
     name_old_mono = path_in_mono.split(".hdf5")[0]
     name_new_0_mono = name_old_mono+"_dim_0_reduced.hdf5"
     name_new_1_mono = name_old_mono+"_dim_1_reduced.hdf5"
-
-    cut_air_slices_by_axis(0, cuts_x, path_in_mono, name_new_0_mono)
+    
+    cut_volume_by_axis(0, cuts_x, path_in_mono, name_new_0_mono)
     os.remove(path_in_mono)
-    cut_air_slices_by_axis(0, cuts_y, name_new_0_mono, name_new_1_mono)
+    cut_volume_by_axis(1, cuts_y, name_new_0_mono, name_new_1_mono)
     os.remove(name_new_0_mono)
-    cut_air_slices_by_axis(0, cuts_z, name_new_1_mono, path_in_mono)
+    cut_volume_by_axis(2, cuts_z, name_new_1_mono, path_in_mono)
     os.remove(name_new_1_mono)
 
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file-path-in-mono", "-f", required=True, type=str,
+    parser.add_argument("--file-path-in-mono", "-fm", required=True, type=str,
                         help="absolut path of input hdf5 file")
 
-    parser.add_argument("--file-path-in-poly", "-f", required=True, type=str,
+    parser.add_argument("--file-path-in-poly", "-fp", required=True, type=str,
                         help="absolut path of input hdf5 file")
 
 
-    parser.add_argument("--file-path-out-mono", "-o", required=False, type=str, 
+    parser.add_argument("--file-path-out-mono", "-om", required=True, type=str, 
                         help="absolut path of output hdf5 file")
     
-    parser.add_argument("--file-path-out-poly", "-o", required=False, type=str, 
+    parser.add_argument("--file-path-out-poly", "-op", required=True, type=str, 
                         help="absolut path of output hdf5 file")
 
 
     args = parser.parse_args()
 
+    
     trans_hdf5_incremental(args.file_path_in_mono, args.file_path_out_mono)
     trans_hdf5_incremental(args.file_path_in_poly, args.file_path_out_poly)
-
+    
     cut_volume(args.file_path_out_poly, args.file_path_out_mono)
     
   
