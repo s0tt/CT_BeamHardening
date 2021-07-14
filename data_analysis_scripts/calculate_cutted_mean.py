ceda554894a8cab57e1f66469783287ee15058c4
kellermx <kellermx@ipvs.uni-stuttgart.de>
2021-07-14 13:34:02 +0200

    calculate cutted mean adaptions

--- data_analysis_scripts/calculate_cutted_mean.py
+++ data_analysis_scripts/calculate_cutted_mean.py
@@ -1,5 +1,13 @@
 import h5py
 import argparse
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(
+    os.path.realpath(__file__)), '../CNN-AI-CT'))
+
+from utils import parse_dataset_paths
+
 
 def add_cutted_mean_grey_value(path_in: str):
     with h5py.File(path_in, "r+") as f_in:
@@ -12,14 +20,23 @@ def add_cutted_mean_grey_value(path_in: str):
             vol_slice = vol_hdf5[:, y_slice, :]
             grey_value_sum += vol_slice.flatten().sum()
             print(grey_value_sum)
-        f_in.attrs['Mean_grey_value_cutted'] = grey_value_sum/vol_hdf5.size
+        
+        # add cutted mean as MetaData
+        vol_hdf5.attrs['Mean_grey_value_cutted_before_rescaling'] = grey_value_sum/vol_hdf5.size
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file-path-in", "-f", required=True, type=str,
-                        help="absolut path of input hdf5 file")
+    parser.add_argument("--file-in", "-f", required=True,
+                        help="Path to json file that contains all datasets")
+    parser.add_argument("--dataset-names", "-dn", required=False, nargs='+', default=["all"],
+                        help="Names of the datasets of --file-in that should analysed")
     args = parser.parse_args()
-    add_cutted_mean_grey_value(args.file_path_in)
+
+    dataset_paths = parse_dataset_paths(args.file_in, args.dataset_names)
+
+    for paths in dataset_paths:
+        add_cutted_mean_grey_value(paths[0])
+        add_cutted_mean_grey_value(paths[1])
 
 
 if __name__ == "__main__":
