bcc3b93238d5fae2d86a4aa671811c0078c910ac
Max Keller <st157951@stud.uni-stuttgart.de>
2021-07-21 14:05:32 +0200

    Adapt RunModel for different Networks

--- models/runModel.py
+++ models/runModel.py
@@ -8,11 +8,13 @@ from pytorch_lightning.plugins import DDPPlugin
 
 from visualization import make_grid, plot_pred_gt, plot_ct
 from CNN_ai_ct import CNN_AICT
+from IRR_CNN_ai_ct import IRR_CNN_AICT
+from Unet import Unet
 from dataloader import CtVolumeData
 from utils import parse_dataset_paths
 
 
-def runModel(chkpt_path, log_path, datasets, nr_test_samples, tensorboard_name, workers):
+def runModel(model, chkpt_path, log_path, datasets, nr_test_samples, tensorboard_name, workers, forward_iterations):
     ct_volumes = CtVolumeData(
         paths=datasets,
         batch_size=5,
@@ -25,7 +27,13 @@ def runModel(chkpt_path, log_path, datasets, nr_test_samples, tensorboard_name, 
         manual_test=nr_test_samples
     )
 
-    model = CNN_AICT.load_from_checkpoint(chkpt_path)
+    if str(model).lower() == "cnn-ai-ct":
+        model = CNN_AICT.load_from_checkpoint(chkpt_path)
+    elif str(model).lower() == "unet":
+        model = Unet.load_from_checkpoint(chkpt_path)
+    elif str(model).lower() == "irr-cnn-ai-ct":
+        model = IRR_CNN_AICT(chkpt_path, forward_iterations=forward_iterations),
+
     model.plot_test_step = nr_test_samples
     model.eval()
     experiment_name = tensorboard_name+"-"+chkpt_path.split("/")[-2]+"-"+chkpt_path.split("/")[-1]
@@ -45,6 +53,8 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--check-point", "-cp", required=True,
                         help="Path to model checkpoint")
+    parser.add_argument("--model", "-m", required=True, default="cnn-ai-ct",
+                        help="model name [cnn-ai-ct, unet, irr-cnn-ai-ct]")
     parser.add_argument("--data-path", "-dp", required=True, type=str,
                         help="Path to dataset json")
     parser.add_argument("--log-dir", "-ld", required=False, default="",
@@ -57,9 +67,12 @@ def main():
                         help="name of datasets to load")
     parser.add_argument("--workers", "-w", required=False, default=8, type=int,
                         help="name of datasets to load")
+    parser.add_argument("--forward-iterations", "-fi", required=False, default=10,
+                        help="Number of forward iterations: See IRR-Networks for details")
     args = parser.parse_args()
     data_paths = parse_dataset_paths(args.data_path, args.dataset_names)
-    runModel(args.check_point, args.log_dir, data_paths, args.test_samples, args.tensorboard_name, args.workers)
+    runModel(args.model, args.check_point, args.log_dir, data_paths, args.test_samples, args.tensorboard_name, args.workers, 
+            args.forward_iterations)
 
 
 if __name__ == "__main__":
